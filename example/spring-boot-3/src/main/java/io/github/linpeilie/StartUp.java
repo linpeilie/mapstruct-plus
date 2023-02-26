c17cbdd3edf61176b4cce0be0a60650e0384a4b3
linpeilie <linpeilie@qq.com>
2023-02-26 14:03:38 +0800

    - 更新spring-boot-start依赖版本为2.7.9，适配springboot3 - 提供springboot3用例

---
+++ example/spring-boot-3/src/main/java/io/github/linpeilie/StartUp.java
@@ -0,0 +1,29 @@
+package io.github.linpeilie;
+
+import io.github.linpeilie.model.Car;
+import io.github.linpeilie.model.CarDto;
+import io.github.linpeilie.model.CarType;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartUp
+    implements InitializingBean {
+
+    private Converter converter;
+
+    @Autowired
+    public void setConverter(final Converter converter) {
+        this.converter = converter;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        final Car car = new Car();
+        car.setType(CarType.OTHER);
+
+        final CarDto carDto = converter.convert(car, CarDto.class);
+        System.out.println(carDto);
+    }
+}
