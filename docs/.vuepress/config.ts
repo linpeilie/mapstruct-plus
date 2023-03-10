3020cfa464aa2e15e778fb93a8d8ae074bc1a03d
linpeilie <linpeilie@qq.com>
2023-03-10 10:41:19 +0800

    add baidu-site-verification

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,15 +7,18 @@ import theme from "./theme.js";
 export default defineUserConfig({
     base: "/",
 
-    head: [['script', {}, `
-        var _hmt = _hmt || [];
-        (function() {
-          var hm = document.createElement("script");
-          hm.src = "https://hm.baidu.com/hm.js?fab881821b3db8a7c460db1c91ea0f3a";
-          var s = document.getElementsByTagName("script")[0];
-          s.parentNode.insertBefore(hm, s);
-        })();
-    `]],
+    head: [
+        ['script', {}, `
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?fab881821b3db8a7c460db1c91ea0f3a";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+        `],
+        ['meta', {name: 'baidu-site-verification', content: 'codeva-0h8mfHFGqT'}]
+    ],
 
     locales: {
         "/": {
