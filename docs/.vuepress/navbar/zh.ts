9e7716d799592322a81f5fb90ca7ef74b7f4a1d3
linpeilie <linpeilie@qq.com>
2023-08-16 10:23:06 +0800

    update docs

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -4,6 +4,6 @@ export const zhNavbar = navbar([
   "/",
   { text: "指南", icon: "api", link: "/guide/class-convert" },
   { text: "更新日志", icon: "blog", link: "/release/log" },
-  { text: "MapStruct中文文档", icon: 'read', link: "/mapstruct/1-5-3-Final"}
+  { text: "MapStruct中文文档", icon: 'read', link: "/mapstruct/1-5-5-Final"}
 ]);
 
