2358e06ded8252f657112bdb6ef9e3827afb29b1
gzoldou <gzoldou@163.com>
2024-02-21 16:31:10 +0800

    增加循环引用测试用例

--- example/spring-boot-with-lombok/src/main/java/io/github/linpeilie/model/Employee.java
+++ example/spring-boot-with-lombok/src/main/java/io/github/linpeilie/model/Employee.java
@@ -2,14 +2,25 @@ package io.github.linpeilie.model;
 
 import io.github.linpeilie.annotations.AutoMapper;
 import java.util.List;
+
+import io.github.linpeilie.annotations.AutoMapping;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @AutoMapper(target = EmployeeDto.class)
 public class Employee {
 
+    @AutoMapping(target = "employeeName", source = "name")
     private String name;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private Employee reportsTo;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private List<Employee> team;
 
 }
--- example/spring-boot-with-lombok/src/main/java/io/github/linpeilie/model/EmployeeDto.java
+++ example/spring-boot-with-lombok/src/main/java/io/github/linpeilie/model/EmployeeDto.java
@@ -1,15 +1,21 @@
 package io.github.linpeilie.model;
 
-import io.github.linpeilie.annotations.AutoMapper;
 import java.util.List;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
-@AutoMapper(target = Employee.class)
 public class EmployeeDto {
 
     private String employeeName;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private EmployeeDto reportsTo;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private List<EmployeeDto> team;
 
 }
