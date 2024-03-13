d34c4176810e501ee410bd70e041c20d7456cd70
linpeilie <linpeilie@qq.com>
2024-03-13 14:52:00 +0800

    CycleAvoidingMappingContext 增加类型作为下标

--- mapstruct-plus/src/main/java/io/github/linpeilie/CycleAvoidingMappingContext.java
+++ mapstruct-plus/src/main/java/io/github/linpeilie/CycleAvoidingMappingContext.java
@@ -1,5 +1,6 @@
 package io.github.linpeilie;
 
+import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import org.mapstruct.BeforeMapping;
@@ -8,16 +9,28 @@ import org.mapstruct.TargetType;
 
 public class CycleAvoidingMappingContext {
 
-    private Map<Object, Object> knownInstances = new IdentityHashMap<Object, Object>();
+    private Map<Object, Map<Class<?>, Object>> knownInstances = new IdentityHashMap<>();
 
     @BeforeMapping
     public <T> T getMappedInstance(Object source, @TargetType Class<T> targetType) {
-        return (T) knownInstances.get( source );
+        Map<Class<?>, Object> map = knownInstances.get(source);
+        if (map == null || map.isEmpty()) {
+            return null;
+        }
+        return (T) map.get(targetType);
     }
 
     @BeforeMapping
     public void storeMappedInstance(Object source, @MappingTarget Object target) {
-        knownInstances.put( source, target );
+        if (target == null) {
+            return;
+        }
+        Map<Class<?>, Object> map = knownInstances.get(source);
+        if (map == null) {
+            map = new HashMap<>();
+        }
+        map.put(target.getClass(), target);
+        knownInstances.put( source, map );
     }
 
 }
