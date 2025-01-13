c064b4442e133d0c31c7eb2db885ffbdabaa547b
秋辞未寒 <545073804@qq.com>
2025-01-13 05:54:07 +0000

    enhance Converter支持Consumer函数
    Signed-off-by: 秋辞未寒 <545073804@qq.com>

--- mapstruct-plus/src/main/java/io/github/linpeilie/Converter.java
+++ mapstruct-plus/src/main/java/io/github/linpeilie/Converter.java
@@ -1,10 +1,13 @@
 package io.github.linpeilie;
 
+import io.github.linpeilie.utils.CollectionUtils;
+import io.github.linpeilie.utils.ObjectUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
 
 public class Converter {
 
@@ -20,77 +23,134 @@ public class Converter {
 
     @SuppressWarnings("unchecked")
     public <S, T> T convert(S source, Class<T> targetType) {
-        if (source == null) {
+        if (ObjectUtils.isAnyNull(source, targetType)) {
             return null;
         }
-        BaseMapper<S, T> mapper = (BaseMapper<S, T>) converterFactory.getMapper(source.getClass(), targetType);
-        if (mapper != null) {
-            return mapper.convert(source);
+        Class<?> sourceType = source.getClass();
+        BaseMapper<S, T> mapper = (BaseMapper<S, T>) converterFactory.getMapper(sourceType, targetType);
+        if (ObjectUtils.isNull(mapper)) {
+            throw new ConvertException("cannot find converter from " + sourceType.getSimpleName() + " to " + targetType.getSimpleName());
         }
-        throw new ConvertException(
-            "cannot find converter from " + source.getClass().getSimpleName() + " to " + targetType.getSimpleName());
+        return mapper.convert(source);
+    }
+
+    public <S, T> T convert(S source, Class<T> targetType, Consumer<T> beanConsumer) {
+        T bean = convert(source, targetType);
+        return ObjectUtils.handleIfNull(bean, beanConsumer);
     }
 
     @SuppressWarnings("unchecked")
     public <S, T> T convert(S source, T target) {
-        if (source == null) {
+        if (ObjectUtils.isAnyNull(source, target)) {
             return null;
         }
-        if (target == null) {
-            return null;
+        Class<?> sourceType = source.getClass();
+        Class<?> targetType = target.getClass();
+        BaseMapper<S, T> mapper = (BaseMapper<S, T>) converterFactory.getMapper(sourceType, targetType);
+        if (ObjectUtils.isNull(mapper)) {
+            throw new ConvertException("cannot find converter from " + sourceType.getSimpleName() + " to " + targetType.getSimpleName());
         }
-        BaseMapper<S, T> mapper = (BaseMapper<S, T>) converterFactory.getMapper(source.getClass(), target.getClass());
-        if (mapper != null) {
-            return mapper.convert(source, target);
-        }
-        throw new ConvertException("cannot find converter from " + source.getClass().getSimpleName() + " to " +
-                                   target.getClass().getSimpleName());
+        return mapper.convert(source, target);
     }
 
-    public <S, T> List<T> convert(List<S> source, Class<T> targetType) {
-        if (source == null || source.isEmpty()) {
-            return new ArrayList<>();
-        }
-        return source.stream().map(item -> convert(item, targetType)).collect(Collectors.toList());
+    public <S, T> T convert(S source, T target, Consumer<T> beanConsumer) {
+        T bean = convert(source, target);
+        return ObjectUtils.handleIfNull(bean, beanConsumer);
     }
 
+    public <S, T> List<T> convert(List<S> sourceList, Class<T> targetType) {
+        return convert(sourceList, targetType, (Consumer<T>) null);
+    }
 
     @SuppressWarnings("unchecked")
-    public <S, T> T convert(S source, Class<T> target, CycleAvoidingMappingContext context) {
-        if (source == null) {
-            return null;
-        }
-        BaseCycleAvoidingMapper<S, T> mapper =
-            (BaseCycleAvoidingMapper<S, T>) converterFactory.getCycleAvoidingMapper(source.getClass(), target);
-        if (mapper != null) {
-            return mapper.convert(source, context);
-        }
-        throw new ConvertException("cannot find converter from " + source.getClass().getSimpleName() + " to " +
-                                   target.getSimpleName());
-    }
-
-    public <S, T> List<T> convert(List<S> source, Class<T> targetType, CycleAvoidingMappingContext context) {
-        if (source == null || source.isEmpty()) {
+    public <S, T> List<T> convert(List<S> sourceList, Class<T> targetType, Consumer<T> beanConsumer) {
+        if (CollectionUtils.isEmpty(sourceList)) {
             return new ArrayList<>();
         }
-        return source.stream().map(item -> convert(item, targetType, context)).collect(Collectors.toList());
+        if (ObjectUtils.isNull(targetType)) {
+            throw new ConvertException("targetType cannot be null");
+        }
+        Class<?> sourceType = sourceList.getFirst().getClass();
+        BaseMapper<S, T> mapper = (BaseMapper<S, T>) converterFactory.getMapper(sourceType, targetType);
+        if (ObjectUtils.isNull(mapper)) {
+            throw new ConvertException("cannot find converter from " + sourceType.getSimpleName() + " to " + targetType.getSimpleName());
+        }
+        // 如果 beanConsumer 本来就为 null，则不再调用带 Consumer 参数的 convert 方法，避免在循环中进行不必要的非空判断
+        if (ObjectUtils.isNotNull(beanConsumer)) {
+            return CollectionUtils.toList(sourceList, source -> {
+                T bean = mapper.convert(source);
+                beanConsumer.accept(bean);
+                return bean;
+            });
+        }
+        return mapper.convert(sourceList);
     }
 
-
-
-    public <T> T convert(Map<String, Object> map, Class<T> target) {
-        if (map == null || map.isEmpty()) {
+    @SuppressWarnings("unchecked")
+    public <S, T> T convert(S source, Class<T> targetType, CycleAvoidingMappingContext context) {
+        if (ObjectUtils.isAnyNull(source, targetType)) {
             return null;
         }
-        if (map.values().stream().allMatch(Objects::isNull)) {
+        Class<?> sourceType = source.getClass();
+        BaseCycleAvoidingMapper<S, T> mapper = (BaseCycleAvoidingMapper<S, T>) converterFactory.getCycleAvoidingMapper(sourceType, targetType);
+        if (ObjectUtils.isNull(mapper)) {
+            throw new ConvertException("cannot find converter from " + sourceType.getSimpleName() + " to " + targetType.getSimpleName());
+        }
+        return mapper.convert(source, context);
+    }
+
+    public <S, T> T convert(S source, Class<T> targetType, CycleAvoidingMappingContext context, Consumer<T> beanConsumer) {
+        T bean = convert(source, targetType, context);
+        return ObjectUtils.handleIfNull(bean, beanConsumer);
+    }
+
+    public <S, T> List<T> convert(List<S> sourceList, Class<T> targetType, CycleAvoidingMappingContext context) {
+        return convert(sourceList, targetType, context, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <S, T> List<T> convert(List<S> sourceList, Class<T> targetType, CycleAvoidingMappingContext context, Consumer<T> beanConsumer) {
+        if (CollectionUtils.isEmpty(sourceList)) {
+            return new ArrayList<>();
+        }
+        if (ObjectUtils.isNull(targetType)) {
+            throw new ConvertException("targetType cannot be null");
+        }
+        Class<?> sourceType = sourceList.getFirst().getClass();
+        BaseCycleAvoidingMapper<S, T> mapper = (BaseCycleAvoidingMapper<S, T>) converterFactory.getCycleAvoidingMapper(sourceType, targetType);
+        if (ObjectUtils.isNull(mapper)) {
+            throw new ConvertException("cannot find converter from " + sourceType.getSimpleName() + " to " + targetType.getSimpleName());
+        }
+
+        // 如果 beanConsumer 本来就为 null，则不再调用带 Consumer 参数的 convert 方法，避免在循环中进行不必要的非空判断
+        if (ObjectUtils.isNotNull(beanConsumer)) {
+            return CollectionUtils.toList(sourceList, source -> {
+                T bean = mapper.convert(source, context);
+                beanConsumer.accept(bean);
+                return bean;
+            });
+        }
+        return mapper.convert(sourceList, context);
+    }
+
+    public <T> T convert(Map<String, Object> map, Class<T> targetType) {
+        if (CollectionUtils.isEmpty(map) || map.values().stream().allMatch(Objects::isNull)) {
             return null;
         }
-        final BaseMapMapper<T> mapper = converterFactory.getMapMapper(target);
-        if (mapper != null) {
-            return mapper.convert(map);
+        if (ObjectUtils.isNull(targetType)) {
+            throw new ConvertException("targetType cannot be null");
         }
-        throw new ConvertException("cannot find converter from " + map.getClass().getName() + " to " +
-                                   target.getSimpleName());
+
+        final BaseMapMapper<T> mapper = converterFactory.getMapMapper(targetType);
+        if (ObjectUtils.isNull(mapper)) {
+            throw new ConvertException("cannot find converter from " + map.getClass().getName() + " to " + targetType.getSimpleName());
+        }
+        return mapper.convert(map);
+    }
+
+    public <T> T convert(Map<String, Object> map, Class<T> targetType, Consumer<T> beanConsumer) {
+        T bean = convert(map, targetType);
+        return ObjectUtils.handleIfNull(bean, beanConsumer);
     }
 
 }
